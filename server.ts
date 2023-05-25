479d0607ce62662da2f32d0606d9d3c350adf3ed
João Pedro Siqueira <joaopfsiqueiraa@gmail.com>
2023-05-25 02:12:43 -0300

    feat: importando app, criando rota e get para testar

---
+++ server.ts
@@ -0,0 +1,9 @@
+import { app } from './app';
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
