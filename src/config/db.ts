722af0f08574d9073bfc6fecc82e1d16efd45f91
João Pedro Siqueira <joaopfsiqueiraa@gmail.com>
2023-05-31 02:08:14 -0300

    feat: criando configuração de bd

---
+++ src/config/db.ts
@@ -0,0 +1,13 @@
+import * as mongoose from 'mongoose';
+
+const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } = process.env;
+const options = {
+  maxPoolSize: 10, // Maintain up to 10 socket connections
+  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+};
+
+mongoose.connect(
+  `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admi`,
+  options
+);
