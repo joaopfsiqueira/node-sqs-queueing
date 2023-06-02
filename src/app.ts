e3cd5601f2878554799c7a1d53ce9f77474be940
João Pedro Siqueira <joaopfsiqueiraa@gmail.com>
2023-06-02 00:38:51 -0300

    fix: ajustando url + importando no apop

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,12 @@ import * as morgan from 'morgan';
 import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config();
 
+import * as mongo from './config/db';
+
 // criando app
 export const app = express();
+
+mongo.connectDb();
 
 // liberando TODO acesso aos serviços.
 app.use(cors());
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,6 @@
 import * as mongoose from 'mongoose';
 
-const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } = process.env;
-const url = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
+const { MONGODB_URI } = process.env; //pegando de dentro do process.env
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -10,20 +9,22 @@ const options = {
   socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
 };
 
-mongoose.connect(url, options);
+export const connectDb = async () => {
+  mongoose.connect(MONGODB_URI, options);
 
-mongoose.connection.on('connected', function () {
-  console.log(
-    `Mongoose default connection open to ${mongoose.connection.host} (${mongoose.connection.name})`
-  );
-});
+  mongoose.connection.on('connected', function () {
+    console.log(
+      `Mongoose default connection open to ${mongoose.connection.host} (${mongoose.connection.name})`
+    );
+  });
 
-mongoose.connection.on('error', function (err) {
-  console.log('Mongoose default connection error: ' + err);
-  process.exit(1);
-});
+  mongoose.connection.on('error', function (err) {
+    console.log('Mongoose default connection error: ' + err);
+    process.exit(1);
+  });
 
-mongoose.connection.on('disconnected', function () {
-  console.log('Mongoose default connection disconnected');
-  process.exit(1);
-});
+  mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose default connection disconnected');
+    process.exit(1);
+  });
+};
