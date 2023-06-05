35750ffd9af282539da4928c80ea32b9393cc1cb
João Pedro Siqueira <joaopfsiqueiraa@gmail.com>
2023-06-05 06:05:47 -0300

    feat: configurando tsconfig e ajustando imports no app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import * as express from 'express';
-import * as cors from 'cors'; // liberando acesso para o front!
+import express from 'express';
+import cors from 'cors'; // liberando acesso para o front!
+import morgan from 'morgan';
 import * as bodyParser from 'body-parser'; // recebendo json e enviando
-import * as morgan from 'morgan';
 import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config();
-
 import * as mongo from './config/db';
+
+dotenv.config();
 
 // criando app
 export const app = express();
