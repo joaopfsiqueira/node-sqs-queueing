943a43a083a4415d368d87eaef18659ec1a1a5be
João Pedro Siqueira <joaopfsiqueiraa@gmail.com>
2023-06-17 16:02:03 -0300

    fix: ajustando para pegar só do process

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,5 @@
 import app from './app';
 
-app.listen(process.env.NODE_DOCKER_PORT || 8080, () => {
-  console.log(
-    'Server is running on port',
-    process.env.NODE_DOCKER_PORT || 8080
-  );
+app.listen(process.env.NODE_DOCKER_PORT, () => {
+  console.log('Server is running on port', process.env.NODE_DOCKER_PORT);
 });
